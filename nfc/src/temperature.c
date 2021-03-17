92e035b3c0abae4c8e36dccc0ca44f266c45911f
pablog33 <pablog33@gmail.com>
2021-03-17 08:22:04 -0300

    se lee la temeperatura en cada trama (10mseg), y luego se muestra el promedio de 200 lecturas. Esto es por la inestabilidad del acondicionador (0-1000mV/0º-100º)

--- nfc/src/temperature.c
+++ nfc/src/temperature.c
@@ -1,16 +1,13 @@
 #include <stdlib.h>
-#include <stdbool.h>
 
-#include "FreeRTOS.h"
-#include "task.h"
 #include "board.h"
 #include "debug.h"
 #include "temperature.h"
 
-#define TEMPERATURE_TASK_PRIORITY ( configMAX_PRIORITIES - 1 )
-
 #define LPC_ADC LPC_ADC0
 #define TEMP_ADC_CH ADC_CH1
+#define TEMPERATURE_COUNT 200
+#define ADC_SAMPLE_RATE 1000
 
 /**
  * @brief 	initializes ADC to read temperature sensor.
@@ -21,7 +18,7 @@ void temperature_init()
 	ADC_CLOCK_SETUP_T adc_setup;
 
 	Chip_ADC_Init(LPC_ADC, &adc_setup);
-	Chip_ADC_SetSampleRate(LPC_ADC, &adc_setup, 1000);
+	Chip_ADC_SetSampleRate(LPC_ADC, &adc_setup, ADC_SAMPLE_RATE);
 	Chip_ADC_EnableChannel(LPC_ADC, TEMP_ADC_CH, ENABLE);
 	Chip_ADC_SetBurstCmd(LPC_ADC, ENABLE);
 }
@@ -33,21 +30,31 @@ void temperature_init()
 uint16_t temperature_read(void)
 {
 	static uint16_t temperature = 0;
-	static TickType_t last_read = 0;
+	static uint16_t actual_temperature = 0;
+	static uint16_t aTemperature[TEMPERATURE_COUNT] = { };
+	static uint16_t i = 0;
 
-	TickType_t now = xTaskGetTickCount();
+	uint16_t sum = 0;
 
-	if ((now - last_read) > pdMS_TO_TICKS(1000)) {
-		/* Waiting for A/D conversion complete */
-		while (Chip_ADC_ReadStatus(LPC_ADC, TEMP_ADC_CH, ADC_DR_DONE_STAT)
-				!= SET) {
-		}
-		/* Read ADC value */
-		Chip_ADC_ReadValue(LPC_ADC, TEMP_ADC_CH, &temperature);
-		/* Print ADC value */
-		lDebug(Info, "LECTURA ADC: %i", temperature);
-		last_read = now;
+	/* Waiting for A/D conversion complete */
+	while (Chip_ADC_ReadStatus(LPC_ADC, TEMP_ADC_CH, ADC_DR_DONE_STAT) != SET) {
 	}
+	/* Read ADC value */
+	Chip_ADC_ReadValue(LPC_ADC, TEMP_ADC_CH, &actual_temperature);
+	aTemperature[i] = actual_temperature;
+
+	++i;
+
+	if (i == TEMPERATURE_COUNT) {
+		for (uint16_t x = 0; x < TEMPERATURE_COUNT; x++) {
+			sum += aTemperature[x];
+		}
+
+		temperature = sum / TEMPERATURE_COUNT;
+		i = 0;
+	}
+	/* Print ADC value */
 
 	return temperature;
+
 }
