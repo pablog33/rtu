5d82c4bb5b9bca263dc207dd0a411a0ead615326
pablog33 <pablog33@gmail.com>
2021-03-22 09:22:23 -0300

    implementando wdt

---
+++ nfc/inc/wdt.h
@@ -0,0 +1,29 @@
+/*
+ * wdt.h
+ *
+ *  Created on: 25 feb. 2021
+ *      Author: gspc
+ */
+
+#ifndef NFC_INC_WDT_H_
+#define NFC_INC_WDT_H_
+
+#ifdef WDT
+#define WDT_ENABLED 1  // code available at runtime
+#ifdef WDT_TEST
+#define WDT_TEST 1  // code available at runtime
+#else
+#define WDT_TEST 0  // all code optimized out
+#endif	/* WDT_TEST */
+#else
+#define WDT_ENABLED 0  // all code optimized out
+#define WDT_TEST 0
+#endif	/* WDT_ENABLED */
+
+void wdt_check(void);
+
+void wdt_test(void);
+
+void din_init(void);
+
+#endif /* NFC_INC_WDT_H_ */
---
+++ nfc/src/wdt.c
@@ -0,0 +1,101 @@
+/*
+ * wdt.c
+ *
+ *  Created on: 25 feb. 2021
+ *      Author: gspc
+ */
+
+#include <stdbool.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "board.h"
+#include "dout.h"
+#include "relay.h"
+#include "wdt.h"
+#include "debug.h"
+
+void wdt_check(void) {
+
+	bool state = false;
+
+	int32_t count = 10;
+	/* Verifica en el arranque, si se ha producido timeout del WDT. En tal caso, parpadeará el led "spare" cada 1 seg */
+	if (Chip_WWDT_GetStatus(LPC_WWDT) & WWDT_WDMOD_WDTOF) {
+		while (--count) {
+			state = !state;
+			relay_spare_led(state);
+			udelay(1000000);
+		}
+	}
+
+	Chip_WWDT_ClearStatusFlag(LPC_WWDT,
+	WWDT_WDMOD_WDTOF | WWDT_WDMOD_WDINT);
+
+}
+
+void vApplicationTickHook(void) {
+
+	if (WDT_TEST) {
+
+		wdt_test();
+	}
+
+	if(Chip_GPIO_GetPinState(LPC_GPIO_PORT, 3, 14)){
+
+		HERE;
+
+	}
+
+	if (WDT_ENABLED) {
+
+		static bool first_cycle = TRUE;
+
+		if (first_cycle) {
+
+			int tc_mseg = 10;
+
+			uint32_t WDT_TO = ((WDT_OSC) / (4 * 1000)) * tc_mseg;
+
+			uint32_t WDT_WINT = WDT_TO / 10;
+
+			uint32_t WDT_WIND = WDT_TO;
+
+			/* Initialize WWDT */
+			Chip_WWDT_Init(LPC_WWDT);
+
+			Chip_WWDT_SetTimeOut(LPC_WWDT, WDT_TO);
+
+			/* -- Reset on WDT_TimeOutFLag -- */
+			Chip_WWDT_SetOption(LPC_WWDT, WWDT_WDMOD_WDRESET);
+
+			/* WINDOW: Max count value from wich a FEED can be credited */
+			Chip_WWDT_SetWindow(LPC_WWDT, WDT_WIND);
+
+			/* WARNING INTERRUPT */
+			Chip_WWDT_SetWarning(LPC_WWDT, WDT_WINT);
+
+			/* Time Out Flag (0x04); Warning flag (0x08) */
+			Chip_WWDT_ClearStatusFlag(LPC_WWDT,
+			WWDT_WDMOD_WDTOF | WWDT_WDMOD_WDINT);
+
+			/* Start watchdog */
+			Chip_WWDT_Start(LPC_WWDT); /* Enable and Feed */
+
+			NVIC_ClearPendingIRQ(WWDT_IRQn);
+
+			NVIC_EnableIRQ(WWDT_IRQn); /* Enable watchdog interrupt */
+
+			Chip_WWDT_Feed(LPC_WWDT);
+			Chip_WWDT_ClearStatusFlag(LPC_WWDT,
+			WWDT_WDMOD_WDTOF | WWDT_WDMOD_WDINT);
+
+			first_cycle = FALSE;
+		} else {
+			Chip_WWDT_Feed(LPC_WWDT);
+			Chip_WWDT_ClearStatusFlag(LPC_WWDT,
+			WWDT_WDMOD_WDTOF | WWDT_WDMOD_WDINT);
+		}
+	}
+}
+
